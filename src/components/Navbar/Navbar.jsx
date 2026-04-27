9d671d76e6a2c11fc7f36be7524395f155e5887a
Junaid-Khan <jkadams227@gmail.com>
2026-04-27 17:58:19 +0530

    one

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import navbarbg2 from "../../assets/navbarbg2.png";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import newlogo from "./../../assets/newlogo.png";
-import newone1 from "./../../assets/newone1.png";
 import newnamelogo1 from "./../../assets/newnamelogo1.png";
 
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
+
   const handleNavClick = () => {
+    setIsMenuOpen(false);
     window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
   };
 
   useEffect(() => {
@@ -18,6 +24,20 @@ function Navbar() {
     };
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 992) {
+        setIsMenuOpen(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
@@ -36,14 +56,19 @@ function Navbar() {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
+          onClick={toggleMenu}
+          aria-controls="navbarNav"
+          aria-expanded={isMenuOpen}
+          aria-label="Toggle navigation"
         >
-          <i className="bi bi-list"></i>
+          <i className={`bi ${isMenuOpen ? "bi-x-lg" : "bi-list"}`}></i>
         </button>
 
         {/* Menu */}
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div
+          className={`collapse navbar-collapse justify-content-end ${isMenuOpen ? "show" : ""}`}
+          id="navbarNav"
+        >
           <ul className="navbar-nav">
 
             <li className="nav-item">
